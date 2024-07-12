6ae95741569d89e9180aa27f6f9e2448b1af2306
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-07-12 00:25:57 -0500

    Scaffold for nextjs project (frontend) Also removing all the boilerplate

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Home",
+};
+
+interface Props {
+	children: React.ReactNode;
+}
+
+function RootLayout({ children }: Readonly<Props>) {
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
+}
+
+export default RootLayout;
