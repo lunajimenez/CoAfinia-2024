2339401e5df79d9c2acc7393ee9fb4503e4d85ad
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-07-20 02:17:28 -0500

    Creating the about page Waiting for the dev's info in order to be rendered in the UI

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,5 +1,64 @@
+import { Content } from "@/components/ui/Content";
+import { RedirectTo } from "@/components/ui/RedirectTo";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Dev } from "@/lib/definitions";
+
+const devs: Dev[] = [
+	{
+		name: "Mauro Gonzalez",
+		description: "College Student",
+		image: "/maurogonzalez.jpg",
+		initials: "MG",
+		content:
+			"Hi! I’m Mauro Gonzalez, a frontend developer. I primarily work with React (Next.js) and TypeScript, along with Tailwind CSS, to create efficient and modern user interfaces. Currently, I’m expanding my skills by learning Nuxt.js (Vue).",
+		links: [
+			{ href: "https://github.com/MauroGonzalez51", label: "Github" },
+		],
+	},
+];
+
 export async function AboutPage() {
-	return <div>AboutPage</div>;
+	return (
+		<Content>
+			<div className="flex gap-4 flex-wrap">
+				{devs.map((dev) => (
+					<Card key={dev.name} className="max-w-96">
+						<CardHeader className="flex items-center flex-row">
+							<Avatar className="size-12">
+								<AvatarImage src={dev.image} />
+								<AvatarFallback>{dev.initials}</AvatarFallback>
+							</Avatar>
+							<div className="ml-4">
+								<CardTitle>{dev.name}</CardTitle>
+								<CardDescription>
+									{dev.description}
+								</CardDescription>
+							</div>
+						</CardHeader>
+						<CardContent className="text-sm">
+							{dev.content}
+						</CardContent>
+						<CardFooter>
+							{dev.links.map((link) => (
+								<RedirectTo key={link.label} href={link.href}>
+									<Button>{link.label}</Button>
+								</RedirectTo>
+							))}
+						</CardFooter>
+					</Card>
+				))}
+			</div>
+		</Content>
+	);
 }
 
 export default AboutPage;
--- frontend/src/lib/definitions.ts
+++ frontend/src/lib/definitions.ts
@@ -1,11 +1,13 @@
-/**
- * TODO:
- *
- * - Write all the schema of the Data
- * - I mean, the variables and stuff
- *
- * -> In order to start writing the UI to filter based on those data and be hable to send it
- * to the backend
- *
- *
- */
+interface DevLink {
+	label: string;
+	href: string;
+}
+
+export interface Dev {
+	name: string;
+	description: string;
+	image: string;
+	initials: string;
+	content: string;
+	links: DevLink[];
+}
