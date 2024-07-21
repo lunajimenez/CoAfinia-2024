de656e8834c54f0e0d9e4acdcd90d0ed82d4541a
mcamilags <94021111+mcamilags@users.noreply.github.com>
2024-07-21 06:06:33 -0500

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -15,6 +15,28 @@ def zip_file():
 
 	return send_file(zip_path, as_attachment=True)
 
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+from plots_pro import *
+from flask import Flask, request, send_file
+
+
+app = Flask(__name__)
+
+
+@app.route("/saber_pro/zip_file", methods=['GET'])
+def zip_file():
+	kwargs = request.args.to_dict()
+	kwargs['period'] = int(kwargs['period'])
+
+	plot_graphs_saber_pro(**kwargs)
+	zip_path = 'static/imgs/saber_pro/imgs.zip'
+
+	return send_file(zip_path, as_attachment=True)
+
 @app.route("/saber_11/zip_file", methods=['GET'])
 def zip_file():
 	kwargs = request.args.to_dict()
